96e23a978396bc8450b08290a3595d633f5a7307
Joao Paulo Felix <joao@safari.com.br>
2020-04-09 20:46:20 -0300

    adjusment filter and colors

--- resources/views/partials/_aside-base.blade.php
+++ resources/views/partials/_aside-base.blade.php
@@ -7,29 +7,29 @@
     <div class="kt-aside-menu-wrapper kt-grid__item kt-grid__item--fluid" id="kt_aside_menu_wrapper" style="margin-top: -50px;">
         <div id="kt_aside_menu" class="kt-aside-menu " data-ktmenu-vertical="1" data-ktmenu-scroll="1" data-ktmenu-dropdown-timeout="500" >
             <ul class="kt-menu__nav">
-                <li class="kt-menu__item {{ in_array($current_page, ['events', 'view-event']) ? 'kt-menu__item--active' : '' }}" aria-haspopup="true" >
-                    <a href="{{ route('events') }}" class="kt-menu__link "><i class="kt-menu__link-icon flaticon2-calendar-6"></i><span class="kt-menu__link-text">Eventos</span></a>
+                <li class="kt-menu__item {{ in_array($current_page, ['events', 'edit-event']) ? 'kt-menu__item--active' : '' }}" aria-haspopup="true" >
+                    <a href="{{ route('events') }}" class="kt-menu__link "><i class="kt-menu__link-icon flaticon2-calendar-6"></i><span class="kt-menu__link-text"><strong>Eventos</strong></span></a>
                 </li>
                 <li class="kt-menu__item {{ in_array($current_page, ['ads', 'add-ad', 'del-ad', 'edit']) ? 'kt-menu__item--active' : '' }} " aria-haspopup="true" >
-                    <a href="{{ route('ads') }}" class="kt-menu__link "><i class="kt-menu__link-icon flaticon2-analytics"></i><span class="kt-menu__link-text">Publicidades</span></a>
+                    <a href="{{ route('ads') }}" class="kt-menu__link "><i class="kt-menu__link-icon flaticon2-analytics"></i><span class="kt-menu__link-text"><strong>Publicidades</strong></span></a>
                 </li>
                 <li class="kt-menu__item {{ in_array($current_page, ['reports']) ? 'kt-menu__item--active' : '' }} " aria-haspopup="true" >
-                    <a href="{{ route('reports') }}" class="kt-menu__link "><i class="kt-menu__link-icon flaticon2-new-email"></i><span class="kt-menu__link-text">Contatos</span></a>
+                    <a href="{{ route('reports') }}" class="kt-menu__link "><i class="kt-menu__link-icon flaticon2-new-email"></i><span class="kt-menu__link-text"><strong>Contatos</strong></span></a>
                 </li>
                 <li class="kt-menu__item {{ in_array($current_page, ['categories', 'add-category', 'edit-category', 'del-category']) ? 'kt-menu__item--active' : '' }} " aria-haspopup="true" >
-                    <a href="{{ route('categories') }}" class="kt-menu__link "><i class="kt-menu__link-icon flaticon2-list-2"></i><span class="kt-menu__link-text">Categorias</span></a>
+                    <a href="{{ route('categories') }}" class="kt-menu__link "><i class="kt-menu__link-icon flaticon2-list-2"></i><span class="kt-menu__link-text"><strong>Categorias</strong></span></a>
                 </li>
                 <li class="kt-menu__item {{ in_array($current_page, ['tags', 'add-tag', 'edit-tag', 'del-tag']) ? 'kt-menu__item--active' : '' }} " aria-haspopup="true" >
-                    <a href="{{ route('tags') }}" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-tags"></i><span class="kt-menu__link-text">Tags</span></a>
+                    <a href="{{ route('tags') }}" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-tags"></i><span class="kt-menu__link-text"><strong>Tags</strong></span></a>
                 </li>
                 <li class="kt-menu__item {{ in_array($current_page, ['users', 'register', 'user-permission', 'del-user', 'edit-user']) ? 'kt-menu__item--active' : '' }} " aria-haspopup="true" >
-                    <a href="{{ route('users') }}" class="kt-menu__link "><i class="kt-menu__link-icon flaticon2-user-1"></i><span class="kt-menu__link-text">Usuários Sistema</span></a>
+                    <a href="{{ route('users') }}" class="kt-menu__link "><i class="kt-menu__link-icon flaticon2-user-1"></i><span class="kt-menu__link-text"><strong>Usuários Sistema</strong></span></a>
                 </li>
                 <li class="kt-menu__item {{ in_array($current_page, ['permissions', 'inactive-route', 'update-routes', 'del-route']) ? 'kt-menu__item--active' : '' }} " aria-haspopup="true" >
-                    <a href="{{ route('permissions') }}" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-unlock"></i><span class="kt-menu__link-text">Permissões</span></a>
+                    <a href="{{ route('permissions') }}" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-unlock"></i><span class="kt-menu__link-text"><strong>Permissões</strong></span></a>
                 </li>
                 <li class="disabled kt-menu__item {{ in_array($current_page, ['notificacoes', 'notificacoes-view']) ? 'kt-menu__item--active' : '' }} " aria-haspopup="true" disabled>
-                    <a href="{{ url('/notificacao') }}" class="kt-menu__link "><i class="kt-menu__link-icon flaticon2-notification"></i><span class="kt-menu__link-text">Notificações</span></a>
+                    <a href="{{ url('/notificacao') }}" class="kt-menu__link "><i class="kt-menu__link-icon flaticon2-notification"></i><span class="kt-menu__link-text"><strong>Notificações</strong></span></a>
                 </li>
             </ul>
         </div>
--- resources/views/partials/_footer-base.blade.php
+++ resources/views/partials/_footer-base.blade.php
@@ -1,5 +1,5 @@
 <!-- begin:: Footer -->
 <div class="kt-footer kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer">
-    <div class="kt-footer__copyright">2019&nbsp;&copy;&nbsp;<a href="http://safari.com/" target="_blank" class="kt-link">Safari</a></div>
+    <div class="kt-footer__copyright">2020&nbsp;&copy;&nbsp;<a href="http://safari.com/" target="_blank" class="kt-link">Safari</a></div>
 </div>
 <!-- end:: Footer -->
